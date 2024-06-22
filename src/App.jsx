1facc720704d01b1ee47149d4e9ddfc484ed201f
Codiadem <chiomaekpemerechi@gmail.com>
2024-06-22 19:21:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-700 font-extrabold ">Welcome to Thrifty!</h1>
+    </>
+  );
+}
+
+export default App;
