c02e25946a6d7a884c5c95cf075e9be076e9917b
Codiadem <chiomaekpemerechi@gmail.com>
2024-07-17 12:03:52 +0100

    chore: install react-router and add page paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import SignupPage from "./components/Pages/SignupPage";
+import Home from "./components/Pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Pages/Login";
 
 function App() {
   return (
     <>
-      <SignupPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
