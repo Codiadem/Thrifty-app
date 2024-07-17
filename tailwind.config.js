e266fa32d3d7ced26f98b022d51a5a78c98f4981
Codiadem <chiomaekpemerechi@gmail.com>
2024-07-17 16:06:09 +0100

    feat:add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // custom colors here
+      primaryBlue: "#371694",
+    },
 
     fontFamily: {
       DMSans: ["DM Sans", "sans-serif"],
     },
-    
 
     extend: {},
   },
