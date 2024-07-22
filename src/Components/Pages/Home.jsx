1f6da7c42b5b526087705f94b956402343b984db
Chingkeziah <chingkeziah@gmail.com>
2024-07-22 09:19:48 +0100

    feat: completed the hero section

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../Layout/Header";
-import Image from "../../../src/images/hero-img-web.png"
+import Image from "../../../src/images/hero-img-web.png";
+
+
 
 function Home() {
   return (
@@ -35,19 +37,40 @@ function Home() {
       {/* a section for recommendations */}
       <section className="recommendations">
         <h3 className="recommended-by">Recomended By</h3>
-        <div>
-          <img src="" alt="kuda" className="kuda" />
-        </div>
-        <div>
-          <img src="" alt="money line with nanacy" className="moneyline" />
-        </div>
-        <div>
-          <img src="" alt="leom technologies" className="leom-tech" />
-        </div>
-        <div>
-          <img src="" alt="tech4dev" className="tech4dev" />
+        <div className="logos">
+          <div>
+            <img src="./../../" alt="logo" />
+          </div>
+          <div>
+            <img src="" alt="money line with nanacy" className="moneyline" />
+          </div>
+          <div>
+            <img src="" alt="leom technologies" className="leom-tech" />
+          </div>
+          <div>
+            <img src="" alt="tech4dev" className="tech4dev" />
+          </div>
         </div>
       </section>
+      {/* why choose us */}
+      <section className="why-choose-us">
+        <div className="text-heading">
+          <h1 className="why-choose-us-text">Why Choose Us</h1>
+        </div>
+          <div className="why-choose-us-content">
+            <img src="./../." />
+            <p className="content">Our team comprises industry-leading professionals with extensive experience and 
+              deep knowledge in our field. This ensures that we provide top-notch solutions 
+              tailored to your specific needs. We are committed to delivering the highest quality 
+              products and services. Our rigorous quality control processes and attention to detail
+              guarantee that you receive only the best. Your satisfaction is our priority.
+              We listen to your needs, understand your goals, and work closely with you 
+              to achieve them. Our personalized service ensures that you feel valued and heard.
+              We stay ahead of the curve by embracing the latest technologies and trends. 
+              Our innovative approach allows us to offer cutting-edge solutions that drive 
+              efficiency and growth for your business.</p>
+          </div>   
+      </section>        
     </>
   );
 }
