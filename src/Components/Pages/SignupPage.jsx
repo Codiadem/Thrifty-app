ff1b11e8692e1318ad21f196ec35c97833cc85a3
Lahitan <harunatawa111@gmail.com>
2024-07-31 14:40:25 +0100

    fix: add the header for mobile view on signup page

--- src/Components/Pages/SignupPage.jsx
+++ src/Components/Pages/SignupPage.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
 import logo from '../../assets/Thrifty-logo.png';
+import logo_mobile from '../../assets/Thrifty-logo-mobile.png'
 import google from '../../assets/Google.png';
 import { NavLink, Link } from "react-router-dom";
 
 const SignupPage = () => {
   return (
       <div>
+
+         {/* header for mobile view */}
+
+
+          <div className=" md:hidden navbar flex justify-between items-center p-1.5">
+                  <Link to="/">
+                    <img src={logo_mobile} alt="logo" className="logo" />
+                  </Link>
+                  <nav>
+                    <ul className="hidden md:flex justify-between space-x-7 lg:space-x-20">
+                      <li className="">
+                        <a href="#home">Home</a>
+                      </li>
+                      <li>
+                        <a href="#about">About</a>
+                      </li>
+                      <li>
+                        <a href="#contact">Contact</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <div className="opt">
+                    <ul className="hidden md:flex space-x-10 items-center">
+                      <li>
+                        <NavLink to="/login">Login</NavLink>
+                      </li>
+                      <li className="border-2 border-primaryBlue rounded p-2">
+                        <NavLink to="/sign-up">Sign Up</NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* hamburger */}
+                  <button className="group space-y-1 group md:hidden">
+                    <div className="w-6 h-1 bg-primaryBlue"></div>
+                    <div className="w-6 h-1 bg-primaryBlue"></div>
+                    <div className="w-6 h-1 bg-primaryBlue"></div>
+                    {/* menu */}
+                    <ul className="bg-primaryBlue w-screen pb-10 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end">
+                      <button className="px-10 py-8 relative ml-auto">
+                        <div className="w-6 h-1 rotate-45 absolute bg-[#FFFFFF]"></div>
+                        <div className="w-6 h-1 -rotate-45 absolute bg-[#FFFFFF]"></div>
+                      </button>
+                      <li className="flex justify-center w-full py-4 hover:bg-[#D9D9D9]">
+                        <a href="#home">Home</a>
+                      </li>
+                      <li className="flex justify-center w-full py-4 hover:bg-[#D9D9D9]">
+                        <a href="#about">About</a>
+                      </li>
+                      <li className="flex justify-center w-full py-4 hover:bg-[#D9D9D9]">
+                        <a href="#contact">Contact</a>
+                      </li>
+                    </ul>
+                  </button>
+            </div>
+
+
             {/*container for Signup page */}
 
             <div className='Container md:flex mx-auto min-h-screen text-center'> 
@@ -15,7 +72,8 @@ const SignupPage = () => {
                 <div className=' w-full md:background md:w-1/2 md:bg-[#371694] md:flex items-center justify-center'>
 
                    <div>
-									  	<img src={logo} alt="" className=" hidden  md:block md:w-[8rem] lg:w-[15rem]" />
+                   <NavLink to="/"><img src={logo} alt="" className=" hidden  md:block md:w-[8rem] lg:w-[15rem]" />
+                   </NavLink>
 									</div>
 
                 </div>
