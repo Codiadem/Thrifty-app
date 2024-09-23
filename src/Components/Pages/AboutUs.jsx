b8933b231af7948a5caef494ae2c03252acf5e55
Chingkeziah <chingkeziah@gmail.com>
2024-09-23 22:22:06 +0100

    feat: imported the header and footer components into the about page

--- src/Components/Pages/AboutUs.jsx
+++ src/Components/Pages/AboutUs.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import Header from '../Layout/Header';
+import Footer from '../Layout/Footer';
 
 function AboutUs() {
   return (
-    <>
-        <div className="about-us">
-           <h1 className='header'>The average thrifty user saves at least 
+    <>  <Header />
+        <div className="">
+           <h1 className=''>The average thrifty user saves at least 
              $300 in 2 months and $4,000 in their first year</h1>
         </div>
+        <Footer />
     </>
   )
 }
