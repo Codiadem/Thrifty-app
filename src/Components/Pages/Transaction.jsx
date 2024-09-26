3d39fc4e5314924296206a807966ae784ffe961c
Lahitan <harunatawa111@gmail.com>
2024-09-26 14:45:50 +0100

    fix: Transaction page

--- src/Components/Pages/Transaction.jsx
+++ src/Components/Pages/Transaction.jsx
@@ -3,7 +3,7 @@ import ExpenseList from './ExpenseList';
 import AddTransaction from './AddTransaction';
 
 const ExpenseTracker = () => {
-  const [transactions, setTransactions] = useState([
+const [transactions, setTransactions] = useState([
     { id: 1, text: 'Purchase of Phone Pouch', amount: -2000, date: '07/09/2023', time: '12:00:18' },
     { id: 2, text: 'Profit from cake', amount: 20000, date: '07/09/2023', time: '12:00:18' }
   ]);
